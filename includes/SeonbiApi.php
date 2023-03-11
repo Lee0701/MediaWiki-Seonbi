334c936a7a7c3ed1c2fb9bdd3909ea690ced68aa
Lee0701 <hgl1002@naver.com>
2023-03-12 02:05:52 +0900

    Add configuration options

--- includes/SeonbiApi.php
+++ includes/SeonbiApi.php
@@ -5,20 +5,25 @@ use MediaWiki\MediaWikiServices;
 class SeonbiApi {
     
     public static function convert($text) {
+        $config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'seonbi' );
+        $apiUrl = $config->get('SeonbiApiUrl');
+
+        $arrow = null;
+        if($config->get('SeonbiArrow')) $arrow = array(
+            'bidirArrow' => $config->get('SeonbiBidirArrow'),
+            'doubleArrow' => $config->get('SeonbiDoubleArrow')
+        );
+
         $postdata = json_encode(
             array(
                 'content' => $text,
                 'contentType' => 'text/html',
-                'quote' => 'CurvedQuotes',
-                'cite' => 'AngleQuotes',
-                'arrow' => array(
-                    'bidirArrow' => true,
-                    'doubleArrow' => true
-                ),
-                'ellipsis' => true,
-                'emDash' => true,
-                'quote' => 'CurvedQuotes',
-                'stop' => 'Vertical'
+                'quote' => $config->get('SeonbiQuote'),
+                'cite' => $config->get('SeonbiCite'),
+                'arrow' => $arrow,
+                'ellipsis' => $config->get('SeonbiEllipsis'),
+                'emDash' => $config->get('SeonbiEmDash'),
+                'stop' => $config->get('SeonbiStop')
             )
         );
         $opts = array('http' =>
@@ -29,8 +34,6 @@ class SeonbiApi {
             )
         );
         $context = stream_context_create($opts);
-        $config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'seonbi' );
-        $apiUrl = $config->get('SeonbiApiUrl');
         $result = file_get_contents($apiUrl, false, $context);
         $json = json_decode($result, true);
         return $json['resultHtml'];
